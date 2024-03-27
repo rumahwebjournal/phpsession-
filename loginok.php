a413d7ab058dc7269ac934abbf84334ea2c04018
rumahwebjournal <165141679+rumahwebjournal@users.noreply.github.com>
2024-03-27 15:55:53 +0700

    loginok.php

--- loginok.php
+++ loginok.php
@@ -5,7 +5,7 @@ session_start();
 // Periksa apakah session telah terdaftar untuk username tersebut
 if(isset($_SESSION['username'])) {
     // Dan jika terdaftar
-    echo 'Selamat Datang, session anda telah terdaftar. Dan hanya akan terhapus jika anda melakukan <a href="http://localhost/logout.php">logout</a>.';
+    echo 'Selamat Datang, ' . $_SESSION['username'] . '! Session anda telah terdaftar. Dan hanya akan terhapus jika anda melakukan <a href="http://localhost/logout.php">logout</a>.';
 } else {
     // Jika tidak terdaftar, kembalikan user ke login.html
     echo '<meta http-equiv="refresh" content="0;url=http://localhost/login.html">';
